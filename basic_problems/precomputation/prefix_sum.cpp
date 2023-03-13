fe560e1c121699ba2f0355a489de8269f207dbf5
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-13 18:33:52 +0530

    simple_update

--- basic_problems/precomputation/prefix_sum.cpp
+++ basic_problems/precomputation/prefix_sum.cpp
@@ -1,5 +1,10 @@
 /*
-Given array a of N inntegers. Given Q queries and in each query given L and R print sum of array elements from index L to R(R,L included)*/
+Given array a of N inntegers. Given Q queries and in each query given L and R print sum of array elements from index L to R(R,L included)
+Constraints
+1 <= N <= 10^5
+1 <= a[i] <= 10^9
+
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
