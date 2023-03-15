f23a5fea6635ebc8cf51f7e74ea320adbe2fbcaa
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-15 21:10:08 +0530

    code_update

--- cses/problems/simple_problem1.cpp
+++ cses/problems/simple_problem1.cpp
@@ -8,6 +8,22 @@ void fast(){
 }
 
 void solve(){
+    int n;
+    cin>>n;
+    while(n--){
+        if(n%3==0 and n%2==0){
+            cout<<"c"<<endl;
+        }
+        else if(n%2==0){
+            cout<<"o"<<endl;
+        }
+        else if(n%3==0){
+            cout<<"d"<<endl;
+        }
+        else{
+            cout<<n<<endl;
+        }
+    }
 
 }
 
