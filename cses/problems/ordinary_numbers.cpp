d1a3c33df6796e9165e03998c26bc9a7ed1df6d4
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-17 23:38:46 +0530

    using_ll

--- cses/problems/ordinary_numbers.cpp
+++ cses/problems/ordinary_numbers.cpp
@@ -47,6 +47,8 @@ For each test case output the number of ordinary numbers among numbers from 1
 
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long int;
+
 
 void fast(){
     ios_base::sync_with_stdio(0); 
