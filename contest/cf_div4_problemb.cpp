bf4d4307dcebc2f55885cc00971fb1f6f4bb51d9
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-19 20:44:44 +0530

    code_update

--- contest/cf_div4_problemb.cpp
+++ contest/cf_div4_problemb.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
 void fast(){
     ios_base::sync_with_stdio(0); 
@@ -11,18 +12,24 @@ void solve(){
     int n;
     cin>>n;
     int a[n];
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
         cin>>a[i];
     }
-    int e=0;
-    int o=0;
-    for(int i=0;i<n;i++){
+    ll e=0;
+    ll o=0;
+    for(ll i=0;i<n;i++){
         if(a[i]%2==0){
             e=e+a[i];
         }
         else{
             o=o+a[i];
         }
+    }
+    if(e>o){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
     }
 
 }
