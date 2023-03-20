73b3315fdf9e952371f5f7be6bfd18a5fc2b29a2
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-20 19:54:16 +0530

    null_pointer_update

--- pointers/deference_operator.cpp
+++ pointers/deference_operator.cpp
@@ -3,6 +3,11 @@ Dereference Operator *
 An interesting property of pointers is that they can be used to access the variable they point to directly. This is done by preceding the pointer name with the dereference operator(*). The operator itself can be read as "value pointed to by"
 */
 
+/*
+NULL pointer:
+Sometimes it is useful to make our point to nothing. This is called a null pointer.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
