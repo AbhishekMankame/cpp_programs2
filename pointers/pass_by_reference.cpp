1d2deaa489ce83c69d65e8a2a4738f836ba79998
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-21 10:28:46 +0530

    all_comment_update

--- pointers/pass_by_reference.cpp
+++ pointers/pass_by_reference.cpp
@@ -1,31 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-void fast(){
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
-}
+// void fast(){
+//     ios_base::sync_with_stdio(0); 
+//     cin.tie(0); 
+//     cout.tie(0);
+// }
 
-// Pass by value
-void applyTax(int income){
-    float tax=0.10;
-    income=income -tax*income;
-}
+// // Pass by value
+// void applyTax(int &income){
+//     float tax=0.10;
+//     income=income - income*tax;
+// }
 
-int main(){
-#ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    freopen("error.txt","w",stderr);
-#endif
+// int main(){
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt","r",stdin);
+//     freopen("output.txt","w",stdout);
+//     freopen("error.txt","w",stderr);
+// #endif
 
-    fast();
-    int income;
-    cin>>income;
-    applyTax(income);
-    cout<<income<<endl;
+//     fast();
+//     int income;
+//     cin>>income;
+//     applyTax(income);
+//     cout<<income<<endl;
 
     
-    return 0;
-}
+//     return 0;
+// }
