68b808b7ad5e42da28f57faa5fb21ad3df32f372
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-21 15:41:46 +0530

    simple_update

--- pointers/2d_dynamic_array.cpp
+++ pointers/2d_dynamic_array.cpp
@@ -16,6 +16,12 @@ int ** create2DArray(int rows, int cols){
     }
 
     // Initialize the array with increasing list of numbers
+    int value=0;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+
+        }
+    }
 }
 
 int main(){
