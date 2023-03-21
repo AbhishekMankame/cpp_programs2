435816dc8463af824e8cd00f0b8dd1ad206992ec
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-21 11:17:21 +0530

    code_update

--- pointers/pass_by_reference_variables.cpp
+++ pointers/pass_by_reference_variables.cpp
@@ -1,16 +1,41 @@
-//#include<bits/stdc++.h>
-#include<iostream>
+// //#include<bits/stdc++.h>
+// #include<iostream>
+// using namespace std;
+
+// void fast(){
+//     ios_base::sync_with_stdio(0); 
+//     cin.tie(0); 
+//     cout.tie(0);
+// }
+
+// // Pass by reference using reference variables
+// void applyTax(int & money){
+//     float tax = 0.10;
+//     money = money - money*tax;
+// }
+
+// int main(){
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt","r",stdin);
+//     freopen("output.txt","w",stdout);
+//     freopen("error.txt","w",stderr);
+// #endif
+
+//     //fast();
+//     int money=1000;
+//     //cin>>income;
+//     applyTax(money);
+//     cout<<money<<endl;
+
+    
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 
-void fast(){
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
-}
-
-// Pass by reference using reference variables
 void applyTax(int & money){
-    float tax = 0.10;
+    float tax=0.10;
     money = money - money*tax;
 }
 
@@ -21,12 +46,9 @@ int main(){
     freopen("error.txt","w",stderr);
 #endif
 
-    //fast();
-    int money=1000;
-    //cin>>income;
-    applyTax(money);
-    cout<<money<<endl;
+    int income;
+    cin>>income;
+    applyTax(income);
+    cout<<income<<endl;
 
-    
-    return 0;
 }
